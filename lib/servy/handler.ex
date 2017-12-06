c1133ff5a25d92eec15f57336e9c76501a618ec9
Ben Booth <bkbooth@gmail.com>
2017-12-06 17:27:26 +1100

    Add a path rewrite from /wildlife to /wildthings

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,10 +2,17 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> rewrite_path
     |> log
     |> route
     |> format_response
   end
+
+  def rewrite_path(%{path: "/wildlife"} = conv) do
+    %{ conv | path: "/wildthings" }
+  end
+
+  def rewrite_path(conv), do: conv
 
   def log(conv), do: IO.inspect conv
 
@@ -119,6 +126,18 @@ IO.puts response
 
 request = """
 DELETE /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /wildlife HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
