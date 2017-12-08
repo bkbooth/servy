1c357546bdc156ceeb83e65cbd83af15cc7a41f3
Ben Booth <bkbooth@gmail.com>
2017-12-08 20:41:35 +1100

    Remove unneeded DELETE handler and test

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -31,8 +31,4 @@ defmodule Servy.BearController do
   def create(conv, %{ "name" => name, "type" => type } = _params) do
     %{ conv | status: 201, resp_body: "Created a #{type} bear named #{name}!" }
   end
-
-  def delete(conv, _params) do
-    %{ conv | status: 403, resp_body: "Deleting a bear is forbidden!"}
-  end
 end
