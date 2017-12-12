84a0928e9217d6f968eebff9ad706a922c385fee
Ben Booth <bkbooth@gmail.com>
2017-12-12 12:54:19 +1100

    Fetch data from a different fake API

--- lib/servy/fetcher.ex
+++ lib/servy/fetcher.ex
@@ -2,10 +2,10 @@ defmodule Servy.Fetcher do
   def async(func) do
     parent = self()
 
-    spawn(fn -> send(parent, {:result, func.()}) end)
+    spawn(fn -> send(parent, {self(), :result, func.()}) end)
   end
 
-  def get_result do
-    receive do {:result, value} -> value end
+  def get_result(pid) do
+    receive do {^pid, :result, value} -> value end
   end
 end
--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -23,18 +23,17 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  def route(%Conv{ method: "GET", path: "/snapshots" } = conv) do
-    Fetcher.async(fn -> VideoCam.get_snapshot("cam-1") end)
-    Fetcher.async(fn -> VideoCam.get_snapshot("cam-2") end)
-    Fetcher.async(fn -> VideoCam.get_snapshot("cam-3") end)
+  def route(%Conv{ method: "GET", path: "/sensors" } = conv) do
+    pid4 = Fetcher.async(fn -> Servy.Tracker.get_location("bigfoot") end)
 
-    snapshot1 = Fetcher.get_result()
-    snapshot2 = Fetcher.get_result()
-    snapshot3 = Fetcher.get_result()
+    snapshots =
+      ["cam-1", "cam-2", "cam-3"]
+      |> Enum.map(&Fetcher.async(fn -> VideoCam.get_snapshot(&1) end))
+      |> Enum.map(&Fetcher.get_result/1)
 
-    snapshots = [snapshot1, snapshot2, snapshot3]
+    where_is_bigfoot = Fetcher.get_result(pid4)
 
-    %{ conv | status: 200, resp_body: inspect snapshots }
+    %{ conv | status: 200, resp_body: inspect { snapshots, where_is_bigfoot } }
   end
 
   def route(%Conv{ method: "GET", path: "/kaboom" }) do
