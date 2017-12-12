c38e30bd63456ce54815065758e8d3db88c54984
Ben Booth <bkbooth@gmail.com>
2017-12-12 15:03:18 +1100

    Move the template render function to a View module

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -3,16 +3,7 @@ defmodule Servy.BearController do
   alias Servy.Wildthings
   alias Servy.Bear
 
-  @templates_path Path.expand("../../templates", __DIR__)
-
-  defp render(conv, template, bindings \\ []) do
-    content =
-      @templates_path
-      |> Path.join(template)
-      |> EEx.eval_file(bindings)
-
-    %{ conv | status: 200, resp_body: content }
-  end
+  import Servy.View, only: [render: 3]
 
   def index(conv) do
     bears =
