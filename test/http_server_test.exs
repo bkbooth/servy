22e3364dfa219743b5d5396204f4a09b730437ea
Ben Booth <bkbooth@gmail.com>
2017-12-12 14:57:24 +1100

    Test multiple handler endpoints

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -6,16 +6,23 @@ defmodule HttpServerTest do
   test "accepts a request on a socket and sends back a response" do
     spawn(HttpServer, :start, [4000])
 
-    url = "http://localhost:4000/wildthings"
+    paths = [
+      "/wildthings",
+      "/wildlife",
+      "/bears",
+      "/bears/1",
+      "/api/bears",
+      "/about",
+    ]
 
-    1..5
-    |> Enum.map(fn(_) -> Task.async(fn -> HTTPoison.get(url) end) end)
+    paths
+    |> Enum.map(fn(path) -> "http://localhost:4000#{path}" end)
+    |> Enum.map(&Task.async(fn -> HTTPoison.get(&1) end))
     |> Enum.map(&Task.await/1)
     |> Enum.map(&assert_successful_response/1)
   end
 
   defp assert_successful_response({:ok, response}) do
     assert response.status_code == 200
-    assert response.body == "Bears, Lions, Tigers"
   end
 end
