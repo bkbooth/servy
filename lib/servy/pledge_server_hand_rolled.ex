5865f98f0a579863d53e79d85bf72eeb690f432f
Ben Booth <bkbooth@gmail.com>
2017-12-13 22:04:54 +1100

    Use handle_other in hand-rolled GenericServer

--- lib/servy/pledge_server_hand_rolled.ex
+++ lib/servy/pledge_server_hand_rolled.ex
@@ -24,9 +24,9 @@ defmodule Servy.GenericServer do
       {:cast, message} ->
         new_state = callback_module.handle_cast(message, state)
         listen_loop(new_state, callback_module)
-      unexpected ->
-        IO.puts "Unexpected message: #{inspect unexpected}"
-        listen_loop(state, callback_module)
+      other ->
+        new_state = callback_module.handle_info(other, state)
+        listen_loop(new_state, callback_module)
     end
   end
 end
@@ -79,6 +79,11 @@ defmodule Servy.PledgeServerHandRolled do
     most_recent_pledges = Enum.take(state, 2)
     new_state = [{ name, amount } | most_recent_pledges]
     {id, new_state}
+  end
+
+  def handle_info(other, state) do
+    IO.puts "Unexpected message: #{inspect other}"
+    state
   end
 
   defp send_pledge_to_service(_name, _amount) do
