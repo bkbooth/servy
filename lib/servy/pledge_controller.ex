2da29240939164962f6d8cd8d7b3df0115155c54
Ben Booth <bkbooth@gmail.com>
2017-12-13 21:58:13 +1100

    Cleanup some extra spaces in PledgeController

--- lib/servy/pledge_controller.ex
+++ lib/servy/pledge_controller.ex
@@ -2,15 +2,12 @@ defmodule Servy.PledgeController do
   import Servy.View
 
   def create(conv, %{ "name" => name, "amount" => amount }) do
-    # Sends the pledge to the external service and caches it
     Servy.PledgeServer.create_pledge(name, String.to_integer(amount))
-
     %{ conv | status: 201, resp_body: "#{name} pledged #{amount}!" }
   end
 
   def index(conv) do
     pledges = Servy.PledgeServer.recent_pledges()
-
     render(conv, "recent_pledges.eex", pledges: pledges)
   end
 
