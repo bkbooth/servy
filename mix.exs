b22c9f45692e803833d89d0661110878c1631b91
Ben Booth <bkbooth@gmail.com>
2017-12-14 15:14:54 +1100

    Add a project description

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule Servy.Mixfile do
   def project do
     [
       app: :servy,
+      description: "A humble HTTP server",
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
